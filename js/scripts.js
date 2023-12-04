27018532d5e32e4ba1919154da4d4fc9db64de43
DomenicoScala <dome.scala12@gmail.com>
2023-12-04 19:27:37 +0100

    FINE SCONTISTICA E STAMPA IN PAGINA

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,37 @@ console.log('kmInNumber', kmInNumber, typeof kmInNumber)
 
 /* RICHIESTA ETA' */
 const richiestaEta = prompt('Quanti anni hai?')
-const EtaInNumber = parseInt(richiestaKm)
+const EtaInNumber = parseInt(richiestaEta)
 
 console.log('richiestaEta', richiestaEta, typeof richiestaEta)
 console.log('EtaInNumber', EtaInNumber, typeof EtaInNumber)
 
-/* CALCOLO PREZZO BASE */
 
 
 
 /* VERIFICO SCONTO DA APPLICARE */
+ if (EtaInNumber < 18){
+    /* CALCOLO PREZZO BASE */
+    const prezzo = (kmInNumber * 0.21)
+
+    const scontoPrezzo = prezzo - ((prezzo / 100) * 20)
+    console.log('scontoPrezzo' , scontoPrezzo, typeof scontoPrezzo)
+    document.querySelector('#result-container').innerHTML = 'Il prezzo del biglietto è €' + scontoPrezzo;
+ }
+
+ if (EtaInNumber > 65){
+    /* CALCOLO PREZZO BASE */
+    const prezzo = (kmInNumber * 0.21)
+
+    const scontoPrezzo = prezzo - ((prezzo / 100) * 40)
+    console.log('scontoPrezzo' , scontoPrezzo, typeof scontoPrezzo)
+    document.querySelector('#result-container').innerHTML = 'Il prezzo del biglietto è €' + scontoPrezzo;
+ }
+ 
+/* ALTRIMENTI */
+ else{
+    const prezzo = (kmInNumber * 0.21)
+    console.log('prezzo', prezzo, typeof prezzo)
+    document.querySelector('#result-container').innerHTML = 'Il prezzo del biglietto è €' + prezzo;
+ }
+ 
