db3b62e538ece7de865f2c211ed700198b22359b
DomenicoScala <dome.scala12@gmail.com>
2023-12-04 18:44:18 +0100

    RICHIESTA KM E RICHIESTA ETA'

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+
+/* RICHIESTA */
+const richiestaKm = prompt('Quanti KM dovrai percorrere?')
+const kmInNumber = parseInt(richiestaKm)
+
+console.log('richiestaKm', richiestaKm, typeof richiestaKm)
+console.log('kmInNumber', kmInNumber, typeof kmInNumber)
+
+/* RICHIESTA ETA' */
+const richiestaEta = prompt('Quanti anni hai?')
+const EtaInNumber = parseInt(richiestaKm)
+
+console.log('richiestaEta', richiestaEta, typeof richiestaEta)
+console.log('EtaInNumber', EtaInNumber, typeof EtaInNumber)
+
+/* CALCOLO PREZZO BASE */
+
+
+
+/* VERIFICO SCONTO DA APPLICARE */
